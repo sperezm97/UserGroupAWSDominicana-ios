96f6d50a6e3484cfc97a8c7fd55b521fc5374150
shailyn <sortizjim@gmail.com>
2020-06-28 15:58:02 -0400

    Initial Commit

---
+++ cuantoes?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cuantoes?
+//
+//  Created by Shailyn javier Ortiz jimenez on 6/28/20.
+//  Copyright © 2020 Shailyn javier Ortiz jimenez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
