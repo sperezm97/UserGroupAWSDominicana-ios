b9ba29ea73c05729dcae3c5766c3fafd970b831c
shailyn <sortizjim@gmail.com>
2020-07-20 02:07:14 -0400

    Removing the src folder since it's no standard on swift projects

--- UserGroupAWSDominicana/src/talks/TalkModels.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TalkModel.swift
-//  cuantoes?
-//
-//  Created by Shailyn javier Ortiz jimenez on 6/29/20.
-//  Copyright © 2020 Shailyn javier Ortiz jimenez. All rights reserved.
-//
-
-import Foundation
---
+++ UserGroupAWSDominicana/talks/TalkApplyView.swift
@@ -0,0 +1,9 @@
+//
+//  TalkApplyView.swift
+//  UserGroupAWSDominicana
+//
+//  Created by Shailyn javier Ortiz jimenez on 7/20/20.
+//  Copyright © 2020 Shailyn javier Ortiz jimenez. All rights reserved.
+//
+
+import Foundation
